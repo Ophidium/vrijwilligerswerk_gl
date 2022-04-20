b28c3258facb23944c4bc714b5f43423d9b0069e
RobinSchoolShit <200512@student.glu.nl>
2022-04-20 14:21:15 +0200

    Login styles af

--- loginPages/login.php
+++ loginPages/login.php
@@ -1,6 +1,6 @@
-<?php
+<!-- <?php
 include '../components/navbar.php';
-?>
+?> -->
 
 <!DOCTYPE html>
 <html lang="en">
@@ -12,10 +12,21 @@ include '../components/navbar.php';
     <link rel="stylesheet" href="../assets/style.css">
 </head>
 <body>
-    <form action="../loggedInPages/index.php" method="POST">
-        <input type="text" placeholder="gebruikersnaam" name="userName">
-        <input type="password" placeholder="wachtwoord" name="userWW">
-        <input type="submit" name="_inloggen" value="Inloggen">
-    </form>    
+    <div class="container">
+        <h2>Inloggen</h2><br>
+    <div class="loginFormContainer">
+        <form action="../loggedInPages/index.php" method="POST">
+            <label for="userName">Gebruikersnaam</label><br>
+            <input class="input" type="text" placeholder="gebruikersnaam" name="userName"><br>
+            <label for="password">Wachtwoord</label><br>
+            <input class="input" type="password" placeholder="wachtwoord" name="userWW"><br>
+            <input class="input" type="submit" name="_inloggen" value="Inloggen">
+            <br>
+            <br>
+            <p>Wachtwoord vergeten?</p>
+        </form>
+    </div><br><br>
+    <p>Heb jij nog geen account? Maak er dan snel een aan</p>
+    </div>
 </body>
 </html>
