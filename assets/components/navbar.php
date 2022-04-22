98b0b01b329addfda52ea4c83c543a2fe4a4c51a
Gavin1103 <gtjin1103@gmail.com>
2022-04-22 09:31:23 +0200

    shit aan het verneuken

--- assets/components/navbar.php
+++ assets/components/navbar.php
@@ -1,26 +1,6 @@
 <?php
 define("BASEURL","http://localhost/programming/vrijwilligerswerk/");
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Navbar</title>
-    <link rel="stylesheet" href="<?php echo BASEURL?>assets/style.css">
-</head>
-<body>
-    <nav>
-        <h1 id="navbarLogo">99</h1>
-        <ul>
-            <li>Home</li>
-            <li>Over ons</li>
-            <li>Contact</li>
-        </ul>
-    </nav>
-</body>
-</html>
 <nav>
     <h1 class="navLink navLinkUnder" id="navbarLogo"><a href="../index.php">99</a></h1>
     <ul>
--- index.php
+++ index.php
@@ -1,30 +1,32 @@
-<?php
-// Page title
-$title = 'Home';
-
-include 'assets/components/navbar.php';
-// Including navbar
-include 'components/navbar.php';
-include 'components/header.php';
-?>
-
 <!-- HTML gedeelte index -->
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Vrijwilligershuis Nieuwegein</title>
-    <link rel="stylesheet" href="../assets/style.css">
+    <link rel="stylesheet" href="assets/style.css">
 </head>
+
 <body>
+    <?php
+    // Page title
+    $title = 'Home';
+
+    // include 'assets/components/navbar.php';
+    // Including navbar
+    include 'assets/components/navbar.php';
+    include 'assets/components/header.php';
+    ?>
     <div class="searchbarContainer">
 
     </div>
 </body>
+
 </html>
 
 <?php // Including navbar
-include 'components/footer.php';
+include 'assets/components/footer.php';
 ?>
