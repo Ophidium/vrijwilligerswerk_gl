9a9082e75ff35db0d79d82cd83c1549054fd31fa
BretsW <59100941+Ophidium@users.noreply.github.com>
2022-04-25 10:39:37 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,22 @@
 <?php
 
 include 'assets/components/navbar.php';
-
-
+<<<<<<< Updated upstream
+=======
+// Including navbar
+include 'assets/components/header.php';
 ?>
+>>>>>>> Stashed changes
+
+
+<<<<<<< Updated upstream
+?>
+=======
+    </div>
+</body>
+</html>
+
+<?php // Including navbar
+include 'assets/components/footer.php';
+?>
+>>>>>>> Stashed changes
